c200f33684564fc05cf6354f65df8336fe629d64
Nikolas Clark <nac09@msn.com>
2025-06-13 15:51:57 -0700

    Getting Todo Data in Console as a new Array using Map

--- app/controllers/TodoController.js
+++ app/controllers/TodoController.js
@@ -1,5 +1,26 @@
+
+
+
+
+import { AppState } from "../AppState.js"
+import { todoService } from "../services/TodoService.js"
+import { getFormData } from "../utils/FormHandler.js"
+import { Pop } from "../utils/Pop.js"
+import { setHTML } from "../utils/Writer.js"
+
 export class TodoController {
   constructor() {
     console.log("TODO CONTROLLER IS HERE")
+    AppState.on('identity', this.getTodos)
+
+  }
+
+  async getTodos() {
+    try {
+      await todoService.getTodos()
+    } catch (error) {
+      Pop.error(error, 'Could not get Todos!')
+      console.error('getTodos failed', error);
+    }
   }
 }
--- app/services/TodoService.js
+++ app/services/TodoService.js
@@ -2,3 +2,15 @@ import { AppState } from "../AppState.js";
 import { ToDo } from "../models/TodoModel.js";
 import { api } from "../utils/Axios.js";
 import { logger } from "../utils/Logger.js";
+
+
+class TodoService {
+  async getTodos() {
+    const response = await api.get('api/todos')
+    console.log('GOT TODOS', response.data);
+    const myTodos = response.data.map(pojo => new ToDo(pojo))
+    logger.log('ToDo', myTodos)
+  }
+}
+
+export const todoService = new TodoService()
